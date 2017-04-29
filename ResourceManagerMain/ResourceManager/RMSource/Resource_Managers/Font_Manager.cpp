dac89481efb3325289c0fca2263c1f8171802af3
jgreen12 <jgreen12@islander.tamucc.edu>
2017-04-28 19:33:52 -0500

    bitches ain't shit but hoes and tricks

--- ResourceManagerMain/ResourceManager/RMSource/Resource_Managers/Font_Manager.cpp
+++
@@ -1,9 +0,0 @@
-#include "Font_Manager.h"
-
-Font_Manager::Font_Manager() //This is where you add new font files. Easier if all placed in Res folder / Fonts folder
-{							//Don't forget to head to the header file too.
-	//example of new font file
-	//addResource(Font_Name::fontExample, "Res/Fonts/Example.ttf");
-
-    addResource(Font_Name::Test, "Res/Fonts/rs.ttf");
-}
--- ResourceManagerMain/ResourceManager/RMSource/Resource_Managers/Sound_Manager.cpp
+++
@@ -1,9 +0,0 @@
-#include "Sound_Manager.h"
-
-Sound_Manager::Sound_Manager() //This is where you add new audio files. Easier if all placed in Res folder / Sounds folder
-{								//Don't forget to head to the header file too.
-	//example of new audio file
-	//addResource(Sound_Name::audioExample, "Res/Sounds/Example.ogg");
-
-    addResource(Sound_Name::Test, "Res/Sounds/Sound.ogg");
-}
